10e566d63e1f8b51dc9a6bb2daae27c821a60e17
Ofir <ofir@veev.com>
2024-05-04 12:27:40 +0300

    Updated game logic

--- matle/src/utils/BoardUtils.js
+++ matle/src/utils/BoardUtils.js
@@ -25,7 +25,87 @@ export function stringToBoard(boardString) {
     return board;    
 }
 
+function getNearestCells(cellRow,cellCol,radius)
+{
+    function isCellValid(cell)
+    {
+        return (cell[0]>0 && cell[0] < 8 && cell[1]>0 && cell[1] < 8)
+    }
+    const res=[]
+    let cell = [cellRow-radius,cellCol-radius];
+    // top row
+        for (let i = 1; i <= radius*2+1; i++) {                
+            if(isCellValid(cell))
+                res.push(cell);
+            cell=[cellRow-radius, cellCol-radius+i]                
+        }
+        cell = [cellRow+radius,cellCol-radius];
+        for (let i = 1; i <= radius*2+1; i++) {                
+            if(isCellValid(cell))
+                res.push(cell);
+            cell=[cellRow+radius, cellCol-radius+i]               
+        }
+        for(let i =1; i< radius*2;i++){
+            let cell = [cellRow-radius+i,cellCol-radius];
+            if(isCellValid(cell))
+                res.push(cell);
+             cell = [cellRow-radius+i,cellCol+radius];
+                if(isCellValid(cell))
+                    res.push(cell);
+        }
+        
+        return res;
+    }
+
 export function getGame()
+{
+    const boardStr = boards[Math.floor(Math.random() * boards.length)];
+    const board = stringToBoard(boardStr)    
+    let blackKingCell=null;
+    let whiteKingCell=null;
+
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+            const piece=board[row][col];
+            if(piece==='wk')
+                whiteKingCell=[row,col]
+            else if(piece==='bk')
+                blackKingCell=[row,col]
+        }            
+    } 
+
+    let hiddenSquares=[getSquareName(whiteKingCell[0],whiteKingCell[1]),getSquareName(blackKingCell[0],blackKingCell[1])]
+    let nearestCells=[]
+    for (let radius = 1; radius <= 2; radius++) {        
+        let nearWhiteKing=getNearestCells(whiteKingCell[0],whiteKingCell[1],radius);
+        nearestCells.push(...nearWhiteKing)        
+    }    
+    for (let radius = 1; radius <= 2; radius++) {        
+        let nearBlackKing=getNearestCells(blackKingCell[0],blackKingCell[1],radius);
+        nearestCells.push(...nearBlackKing)        
+    }    
+    nearestCells=shuffleArray(nearestCells);
+    for (let i = 0; i < Math.min(3,nearestCells.length); i++) {
+        hiddenSquares.push(getSquareName(nearestCells[i][0],nearestCells[i][1]));
+    }
+
+
+    return {board:board, hiddenSquares:hiddenSquares}
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        // Generate a random index lower than the current position
+        let j = Math.floor(Math.random() * (i + 1));
+        
+        // Swap elements at indices i and j
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+
+export function getGame2()
 {
     const boardStr = boards[Math.floor(Math.random() * boards.length)];
     const board = stringToBoard(boardStr)
@@ -57,11 +137,11 @@ export function getGame()
           const col = randomIndex(numCols);
           
           if (shouldSelectKing() && (board[row][col] === 'wk' || board[row][col] === 'bk')) {
-              hiddenSquares.push(`${String.fromCharCode(97 + col)}${numRows - row}`);
+              hiddenSquares.push(getSquareName(col, row));
           } else if (shouldSelectAdjacent() && isAdjacentToKing(row, col)) {
-              hiddenSquares.push(`${String.fromCharCode(97 + col)}${numRows - row}`);
+              hiddenSquares.push(getSquareName(col, row));
           } else if (shouldSelectNull() && board[row][col] === '') {
-              hiddenSquares.push(`${String.fromCharCode(97 + col)}${numRows - row}`);
+              hiddenSquares.push(getSquareName(col, row));
           }
       }
   
@@ -70,4 +150,8 @@ export function getGame()
   
   let hiddenSquares = generateHiddenSquares(board);
   return {board:board, hiddenSquares:hiddenSquares}
+}
+
+function getSquareName(row, col) {
+    return `${String.fromCharCode(97 + col)}${8 - row}`;
 }
